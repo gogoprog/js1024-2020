eb77953084f91632b4a00414f24349717723f727
Gauthier Billot <gogoprog@gmail.com>
2020-07-07 23:03:14 +0200

    Use of shim

--- src/Main.hx
+++ src/Main.hx
@@ -1,13 +1,18 @@
 import js.Browser.window;
 
+@:native("")
+extern class Shim {
+    @:native("a") static var canvas:js.html.CanvasElement;
+    @:native("c") static var context:js.html.CanvasRenderingContext2D;
+}
+
 class Main {
     static function main() {
         var w = window;
-        var c:js.html.CanvasElement = cast w.document.querySelector("canvas");
+        trace(Shim.canvas);
+        untyped __js__("console.log(a);");
         var screenSize = 512;
-        c.width = c.height = screenSize;
-        var ctx:js.html.CanvasRenderingContext2D = c.getContext("2d");
-        var lastFireTime:Int;
+        Shim.canvas.width = Shim.canvas.height = screenSize;
         var rseed;
         var mx= 0;
         var time:Int = 0;
@@ -17,33 +22,18 @@ class Main {
         var cos = m.cos;
         var horizon:Float = 512 * 0.75;
         function col(n:Dynamic) {
-            ctx.fillStyle = n;
+            Shim.context.fillStyle = n;
         }
         function alpha(n) {
-            ctx.globalAlpha = n;
-        }
-        function scale(s) {
-            ctx.scale(s, s);
+            Shim.context.globalAlpha = n;
         }
         function drawRect(x:Float, y:Float, w:Float, h:Float) {
-            ctx.fillRect(x-w/2, y-h/2, w, h);
-        }
-        function mto(x, y) {
-            ctx.moveTo(x, y);
-        }
-        function lto(x, y) {
-            ctx.lineTo(x, y);
-        }
-        function beginPath() {
-            ctx.beginPath();
-        }
-        function fill() {
-            ctx.fill();
+            Shim.context.fillRect(x-w/2, y-h/2, w, h);
         }
         function drawCircle(x, y, r) {
-            beginPath();
-            ctx.arc(x, y, r, 0, 6.28);
-            fill();
+            Shim.context.beginPath();
+            Shim.context.arc(x, y, r, 0, 6.28);
+            Shim.context.fill();
         }
         function random():Float {
             var x = (sin(rseed++) + 1) * 9999;
@@ -65,40 +55,40 @@ class Main {
 
             return n;
         }
-        var gradient = ctx.createLinearGradient(0, 0, 0, screenSize);
+        var gradient = Shim.context.createLinearGradient(0, 0, 0, screenSize);
         gradient.addColorStop(0, "#116");
         gradient.addColorStop(1, "#A9D");
         function loop(t:Float) {
-            // col("#33B");
             col(gradient);
             drawRect(256, 256, screenSize, screenSize);
             alpha(1);
-            rseed = 1;
-            col("#fff");
-            // for(i in 0...99) {
-            //     drawRect(random() * screenSize, (random() * screenSize + t * (random() * 0.2)) % screenSize, 2, 2);
-            // }
             col("#6bf");
             drawCircle(screenSize/2, horizon, 128);
             col("#57c");
+            var a = (time/2) % 32;
 
             for(i in 0...20) {
                 var h = 24 + random() * 40;
-                drawRect(i * 32 - mx * 0.05, horizon - h/2, 16 + random() * 16, h);
+                rseed = Std.int(time/2/32) + i;
+                drawRect(i * 32 - a, horizon - h/2, 16 + random() * 16, h);
             }
 
-            col("#128");
-            drawRect(screenSize/2, horizon, screenSize, 16);
+            {
+                col("#128");
+                drawRect(screenSize/2, horizon, screenSize, 16);
+                var a = time%32;
 
-            for(i in 0...20) {
-                drawRect(i * 32 - mx * 0.1, horizon, 16 + random() * 16, 48 + random() * 64);
+                for(i in 0...20) {
+                    rseed = Std.int(time/32) + i;
+                    drawRect(i * 32 - a, horizon, 16 + random() * 16, 48 + random() * 64);
+                }
             }
 
             col("#aaa");
             alpha(0.4);
             drawRect(256, horizon + 64, screenSize, 128);
             time++;
-            w.requestAnimationFrame(loop);
+            untyped requestAnimationFrame(loop);
         }
         loop(0);
     }
